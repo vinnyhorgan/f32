b56f98329650b82d9db8fc4a90f75ba436578f5c
dvh <vinnyhorgan@gmail.com>
2026-02-07 00:45:45 +0100

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
